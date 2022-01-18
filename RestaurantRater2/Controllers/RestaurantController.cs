a7f418ad0c8317ed2c28106bdfcf16a898e7d221
koleahmantle <koleahmantle@gmail.com>
2022-01-18 13:39:27 -0500

    create view added

--- RestaurantRater2/Controllers/RestaurantController.cs
+++ RestaurantRater2/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,32 @@ namespace RestaurantRater2.Controllers
 {
     public class RestaurantController : Controller
     {
-        // GET: Restaurant
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        
+        // GET: Restaurant/Index
         public ActionResult Index()
         {
+            return View(_db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //Post: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
